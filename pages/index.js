3f21657b6f5957be81ef2752a5f8d085eb2b8cd8
nebelmeerwanderer <stehr.nielsen@gmail.com>
2022-08-18 10:45:55 +0200

    adjusted text field

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (Dette er - This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js  tutorial</a>.)
         </p>
       </section>
 
