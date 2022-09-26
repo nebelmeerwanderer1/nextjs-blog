662dda1ab4b757f7dbfc3925a3e6b056db7b019b
nebelmeerwanderer1 <stehr.nielsen@gmail.com>
2022-09-26 09:12:37 +0200

    mmm

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (dette er This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js  tutorial</a>.)
         </p>
       </section>
